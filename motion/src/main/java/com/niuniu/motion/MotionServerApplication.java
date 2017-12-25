96805a3ac37a16722bfc2c11eb69d9ba4d5d3e42
xuweidong@dajiazhongyi.com <xuweidong@dajiazhongyi.com>
2017-12-25 16:20:13 +0800

    init

---
+++ motion/src/main/java/com/niuniu/motion/MotionServerApplication.java
@@ -0,0 +1,12 @@
+package com.niuniu.motion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotionServerApplication.class, args);
+    }
+}
---
+++ motion/src/main/java/com/niuniu/motion/server/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package com.niuniu.motion.server.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.niuniu.motion.dto.GreetingDTO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public GreetingDTO greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new GreetingDTO(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
